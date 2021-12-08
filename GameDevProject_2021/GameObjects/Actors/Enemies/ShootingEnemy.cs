af6da4944fe720f5830a56ea0429be4625548b77
Thomas <thomas.vancampen@hotmail.com>
2021-12-08 15:35:32 +0100

    cleanup

--- GameDevProject_2021/GameObjects/Actors/Enemies/ShootingEnemy.cs
+++ GameDevProject_2021/GameObjects/Actors/Enemies/ShootingEnemy.cs
@@ -10,12 +10,15 @@ namespace GameDevProject_2021.GameObjects.Actors.Enemies
 {
     class ShootingEnemy : Enemy
     {
+        #region prop and var
         public float RunDistance { get; set; }
         public float RunDistanceCounter { get; set; }
 
         public bool IsShooting { get; set; }
+        public int ShootingTimer { get; set; }
+        #endregion
 
-
+        #region constructors
         public ShootingEnemy(Dictionary<string, Animation> hunterAnimations)
         {
             this.Animations = hunterAnimations;
@@ -25,14 +28,17 @@ namespace GameDevProject_2021.GameObjects.Actors.Enemies
             this.RunDistanceCounter = RunDistance;
             this.Speed = 0.5f;
             this.IsShooting = false;
+            this.ShootingTimer = 4;
         }
+        #endregion
 
+        #region methods
         public override void Update(GameTime gameTime, List<GameObject> gameObjects)
         {
             base.Update(gameTime, gameObjects);
             Move(gameObjects);
             
-            if (gameTime.TotalGameTime.Seconds%4 == 0)
+            if (gameTime.TotalGameTime.Seconds%this.ShootingTimer == 0)
             {
                 AnimationManager.Play(Animations["Shoot"]);
                 this.Movement = Vector2.Zero;
@@ -54,5 +60,6 @@ namespace GameDevProject_2021.GameObjects.Actors.Enemies
         {
             _movementManager.Move(this, gameObjects);
         }
+        #endregion
     }
 }
